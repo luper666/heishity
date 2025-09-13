13a4caba44a7863dd6ddb99ca4b08b75fbb22bfd
luper666 <161530716+luper666@users.noreply.github.com>
2025-09-13 16:37:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // V-- 新增部分 --V
   basePath: '/heishity',
-  assetPrefix: '/heishity/',
-  // ^-- 新增部分 --^
+  assetPrefix: '/heishity', // 注意：末尾的斜杠 / 已被移除，这是更规范的写法
+
+  // V-- 这是新增的、解决问题的关键配置 --V
+  images: {
+    unoptimized: true,
+  },
+  // ^-- 新增部分结束 --^
 };
 
 module.exports = nextConfig;
