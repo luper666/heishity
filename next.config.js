bfa9b3677ba211b97005e651d1abaced87730398
luper666 <161530716+luper666@users.noreply.github.com>
2025-09-13 17:19:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  // 核心配置：必须明确告诉Next.js网站的最终家在一个子目录里
+  basePath: '/heishity',
+  assetPrefix: '/heishity',
+
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
