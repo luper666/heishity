55ce70179d265bac01c2923ff668a91a86f94d9e
luper666 <161530716+luper666@users.noreply.github.com>
2025-09-13 17:12:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   output: 'export',
-  // 核心配置：仅在生产环境（部署时）添加前缀
-  basePath: isProd ? '/heishity' : '',
-  assetPrefix: isProd ? '/heishity/' : '',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
